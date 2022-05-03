04608ae88b217d935c5796d5240dc2ba66258d0a
Filip <filip.rak.20001@gmail.com>
2022-05-03 23:09:21 +0200

    Commit : token system update

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -14,13 +14,14 @@ import java.util.Map;
 
 public class MainWindow extends JFrame {
     //variables
-    private final String token = "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";
     private List<Map<String, Long>> maps;
     private JTextField tf_repositoryName;
     private JTable jt_repositorySelect;
     private JTable jt_repositoryDetail;
     private JPanel mainWindow;
     private JButton jb_dataButton;
+    private JTextField tf_token;
+
     //constructor
     private MainWindow(String title) {
         super(title);
@@ -81,6 +82,7 @@ public class MainWindow extends JFrame {
         @Override
         public void actionPerformed(ActionEvent e) {
             String string_username = getTf_repositoryName().getText();
+            String token = tf_token.getText();
             try {
                 GitHub git = GitHub.connectUsingOAuth(token);
                 GHUser user = git.getUser(string_username);
